9b54acbd4c5b77cf65ce4cd08ddf27d7bb435dea
Gunnerson <brandongunnerson@gmail.com>
2019-03-29 23:30:31 -0700

    making sure changes get pushed

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
-    var Random=Math.floor(Math.random()*101+19)
     // Random number between 19 - 120 selected at the start of game
-
-    $('#randomNumber').text(Random);
+    var Random=Math.floor(Math.random()*101+19)
     // Append random number to the randomNumber id index.html
-
+    $('#randomNumber').text(Random);
+    
+    // Generate random numbers between 1 - 12 for each crystal
     var num1= Math.floor(Math.random()*11+1)
     var num2= Math.floor(Math.random()*11+1)
     var num3= Math.floor(Math.random()*11+1)
     var num4= Math.floor(Math.random()*11+1)
-    // Generate random numbers between 1 - 12 for each crystal
+    
  
     var userTotal= 0; 
     var wins= 0;
@@ -39,6 +39,7 @@ $(document).ready(function(){
     $('#numberWins').text(wins);
     reset();
   }
+  
   //Number of losses
   function loser(){
   alert ("You lose!");
@@ -46,6 +47,7 @@ $(document).ready(function(){
     $('#numberLosses').text(losses);
     reset()
   }
+
   //Event click for jewel
     $('#one').on ('click', function(){
       userTotal = userTotal + num1;
@@ -59,6 +61,7 @@ $(document).ready(function(){
             loser();
           }   
     })  
+
     $('#two').on ('click', function(){
       userTotal = userTotal + num2;
       console.log("New userTotal= " + userTotal);
@@ -70,18 +73,21 @@ $(document).ready(function(){
             loser();
           } 
     })  
+
     $('#three').on ('click', function(){
       userTotal = userTotal + num3;
       console.log("New userTotal= " + userTotal);
       $('#finalTotal').text(userTotal);
-  //sets win/lose conditions
+  
+      //sets win/lose conditions
             if (userTotal == Random){
             winner();
           }
           else if ( userTotal > Random){
             loser();
           } 
-    })  
+    }) 
+
     $('#four').on ('click', function(){
       userTotal = userTotal + num4;
       console.log("New userTotal= " + userTotal);
